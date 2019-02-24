5683621b9572cef9fd709ae738d0aa9263591389
Yue Pi <42617039+SherryPi@users.noreply.github.com>
2019-02-24 15:59:54 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const port = 5000;
 
-http.createServer((req, res)=>{
+http.createServer((req, res)=>{ //create http server
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     var r = require("rethinkdb");
@@ -9,20 +9,18 @@ http.createServer((req, res)=>{
     //var obj1 = {host: '34.204.183.169', port: 28015, db: 'Weatherwayz', user: 'rethinkdb', password: 'weatherwayz'};
     var obj2 = {host: '34.204.183.169', port: 28015, user: 'admin'};
     r.connect( obj2, function(err, conn) {
-    if (err){
-        console.log("cannot connect");
-        console.log(err);
-        throw err;
+        if (err){
+            console.log("cannot connect");
+            console.log(err);
+            throw err;
+        }
+        connection = conn;
 
-    }
-    connection = conn;
-
-    r.db("Weatherwayz").table('Users').
-    run(connection, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result._responses, null, 2));
+        r.db("Weatherwayz").table('Users').
+        run(connection, function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result._responses, null, 2));
+        });
     });
-    });
-
     
-}).listen(port);
+}).listen(port); //server object listen on port 5000;
