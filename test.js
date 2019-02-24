429c1fc7fe842b92ee33cfcb4d5cb31f7f464c64
Yue Pi <42617039+SherryPi@users.noreply.github.com>
2019-02-24 15:54:39 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const port = 5000;
+
+http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    var r = require("rethinkdb");
+    var connection = null;
+    //var obj1 = {host: '34.204.183.169', port: 28015, db: 'Weatherwayz', user: 'rethinkdb', password: 'weatherwayz'};
+    var obj2 = {host: '34.204.183.169', port: 28015, user: 'admin'};
+    r.connect( obj2, function(err, conn) {
+    if (err){
+        console.log("cannot connect");
+        console.log(err);
+        throw err;
+
+    }
+    connection = conn;
+
+    r.db("Weatherwayz").table('Users').
+    run(connection, function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result._responses, null, 2));
+    });
+    });
+
+    
+}).listen(port);
